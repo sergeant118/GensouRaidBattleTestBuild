52b5e50611eaf811076af79c79ae8a89ab2fb4b7
Tatsuya Fujii <example@gmail.com>
2026-06-08 17:19:36 +0900

    AllyPreset Update

--- Build/GensouRaidBattleTestBuild.loader.js
+++ Build/GensouRaidBattleTestBuild.loader.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0c8d9ca97b1c7cdaa78aa5bf12484746451687d77f0410cd0acc7f4caf857cd
+oid sha256:4fccbb84ebe244f602bec4fb2d4a23beca8d50803aaa310b894d8c7769793c88
 size 26982
